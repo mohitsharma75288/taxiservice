4f8dcbe11275e698a8c4c3cc13e66af40bf02bc8
mohit sharma <sharmamohit@gmail.com>
2018-11-23 11:16:31 +0530

    modify further

--- taxiservice/settings.py
+++ taxiservice/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yhotdehcdsmt!5+gol-1(&s+(rdo176v_3f4bzj0ngymx+#r&#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =   [127.0.0.1,'localhost', 'http://taxiserviceapp.herokuapp.com/','https://taxiserviceapp.herokuapp.com/']
+ALLOWED_HOSTS =   ['*', '127.0.0.1', 'localhost', 'http://taxiserviceapp.herokuapp.com/' 'https://taxiserviceapp.herokuapp.com/']
 # Application definition
 
 INSTALLED_APPS = [
