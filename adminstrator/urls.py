c76e0138c2ce8173a472bf56889e9de44c9d3361
mohit sharma <sharmamohit@gmail.com>
2018-12-01 08:21:26 +0530

    jmxjmjx

--- adminstrator/urls.py
+++ adminstrator/urls.py
@@ -6,11 +6,7 @@ urlpatterns = [
     url('', views.dashboard, name='dashboard'),
     url('maps', views.maps, name='maps'),
     url('carlist', views.carlist, name='carlist'),
-    url('highchart', views.charts, name='charts'),
-
-    url('booked', views.booked, name='booked'),
-    url('booking',views.booking, name='booking'),
-    url('signup', views.signup, name='signup'),
+   
     url('typography', views.typography, name='typography'),
     url('table', views.table, name='table'),
     url('notifications', views.notifications, name='notifications'),
