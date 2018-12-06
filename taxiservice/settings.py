064a6e32284dbf9610080bea91624bad2a0cb946
mohitsharma75288 <43772024+mohitsharma75288@users.noreply.github.com>
2018-12-06 13:37:09 +0530

    Update settings.py

--- taxiservice/settings.py
+++ taxiservice/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yhotdehcdsmt!5+gol-1(&s+(rdo176v_3f4bzj0ngymx+#r&#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS =   ['*', '127.0.0.1', 'localhost', 'http://taxiserviceapp.herokuapp.com/' 'https://taxiserviceapp.herokuapp.com/']
 # Application definition
